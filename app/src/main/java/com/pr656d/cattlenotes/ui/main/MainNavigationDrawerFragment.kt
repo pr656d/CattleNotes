e7fcbbdc1b0997ad764f38b4d2b32abed0ce6dc9
Prem Patel <prempatel40p@outlook.com>
2019-11-10 15:53:54 +0530

    Add: Open-source licenses activity

--- app/src/main/java/com/pr656d/cattlenotes/ui/main/MainNavigationDrawerFragment.kt
+++ app/src/main/java/com/pr656d/cattlenotes/ui/main/MainNavigationDrawerFragment.kt
@@ -1,9 +1,11 @@
 package com.pr656d.cattlenotes.ui.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.pr656d.cattlenotes.R
 import com.pr656d.cattlenotes.shared.utils.display.Toaster
@@ -29,21 +31,25 @@ class MainNavigationDrawerFragment : BottomSheetDialogFragment() {
 
         nvMain.apply {
             setNavigationItemSelectedListener { menuItem ->
-                when (menuItem.itemId) {
+                val result = when (menuItem.itemId) {
                     R.id.itemSettings -> {
                         Toaster.show(context, "Settings")
                         true
                     }
                     R.id.itemAboutApp -> {
-                        Toaster.show(this.context, "About this app")
+                        Toaster.show(context, "About this app")
                         true
                     }
                     R.id.itemOpenSourceLicenses -> {
-                        Toaster.show(this.context, "Open-source licenses")
+                        OssLicensesMenuActivity.setActivityTitle(getString(R.string.menu_open_source_licences))
+                        startActivity(Intent(context, OssLicensesMenuActivity::class.java))
                         true
                     }
                     else -> false
                 }
+                if (result) dismiss()
+
+                result
             }
         }
     }
