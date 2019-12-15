cbaed88a53628ab219a479ff135b29ef85915538
Prem Patel <prempatel40p@outlook.com>
2019-12-15 14:22:09 +0530

    Forget to delete shared element code from AddCattleFragment.

--- app/src/main/java/com/pr656d/cattlenotes/ui/main/cattle/add/AddCattleFragment.kt
+++ app/src/main/java/com/pr656d/cattlenotes/ui/main/cattle/add/AddCattleFragment.kt
@@ -1,9 +1,7 @@
 package com.pr656d.cattlenotes.ui.main.cattle.add
 
-import android.os.Bundle
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.transition.TransitionInflater
 import com.google.android.material.snackbar.Snackbar
 import com.pr656d.cattlenotes.R
 import com.pr656d.cattlenotes.ui.main.cattle.BaseCattleFragment
@@ -22,13 +20,6 @@ class AddCattleFragment : BaseCattleFragment() {
     override fun getBaseCattleViewModel(): BaseCattleViewModel = viewModel
 
     override fun provideLayoutId(): Int = R.layout.fragment_add_cattle
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val transition = TransitionInflater.from(context).inflateTransition(android.R.transition.move)
-        sharedElementEnterTransition = transition
-        sharedElementReturnTransition = transition
-    }
 
     override fun setupObservers() {
         super.setupObservers()
